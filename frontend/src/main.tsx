89b3a8437a5e136a2de14bb9adac1064d04ae56d
Kalson <C_ibeste@gcn.ab-inbev.com>
2025-09-06 07:11:45 +0100

    FE:CHORE- UI Implementation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './pages/App.tsx'
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import "./index.css";
+import router from "./route.ts";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <MantineProvider>
+      <RouterProvider router={router} />
+    </MantineProvider>
+  </StrictMode>
+);
--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,5 +1,13 @@
+import { Title } from "@mantine/core";
+
 function App() {
-  return <h1 className="text-red-600 text-7xl">Hello World</h1>;
+  return (
+    <section className="container mx-auto p-24 h-screen">
+      <div>
+        <Title order={1}>Users</Title>
+      </div>
+    </section>
+  );
 }
 
 export default App;
