27b9e751c293db1eee7bfef255b2d5d7a33ab93b
Kalson <C_ibeste@gcn.ab-inbev.com>
2025-09-08 12:40:20 +0100

    cleanups: ReadMe and Test Cases

--- frontend/src/lib/config/axios.ts
+++ frontend/src/lib/config/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, type InternalAxiosRequestConfig } from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:3001",
   headers: {
     "Content-Type": "application/json",
   },
