ada42771788c42a5606204f3fb8aa26c4d6b620c
Stephen Ibe <saintkalson@gmail.com>
2025-09-09 13:08:43 +0100

    truncate long text in post card

--- frontend/src/components/organisms/posts/PostCard.tsx
+++ frontend/src/components/organisms/posts/PostCard.tsx
@@ -12,18 +12,21 @@ type Props = {
 
 export const PostCard = ({ post, deletePost }: Props) => {
   return (
-    <div className="min-h-[293px] border-2 border-[#D5D7DA] rounded-xl shadow-md p-10 relative">
+    <div
+      className="h-[293px] border-2 border-[#D5D7DA] rounded-xl shadow-md p-10 relative flex flex-col justify-between"
+      style={{ maxWidth: 400 }}
+    >
       <div
         className="absolute ml-auto w-fit right-3 top-3 cursor-pointer hover:bg-gray-100 hover:p-2 p-2 hover:rounded-full"
         onClick={deletePost}
       >
         <LuTrash2 color="red" />
       </div>
-      <div className="relative flex flex-col gap-y-4">
-        <Title order={3} className="font-semibold">
+      <div className="relative flex flex-col gap-y-4 h-full">
+        <Title order={3} className="font-semibold truncate">
           {post.title}
         </Title>
-        <Text size="md" className="line-clamp-6">
+        <Text size="md" lineClamp={5}>
           {post.body}
         </Text>
       </div>
